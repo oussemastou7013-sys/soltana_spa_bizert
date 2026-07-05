8b8ba399e21c837e13c34fd57e52262732776776
oussemastou7013-sys <oussemastou7013@gmail.com>
2026-07-05 01:26:42 +0100

    Updated index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: false,
+    watch: {
+      ignored: ['**/dist/**', '**/node_modules/**', '**/.git/**', '**/SPA-SULTANA-main.zip'],
+    },
+  },
 });
